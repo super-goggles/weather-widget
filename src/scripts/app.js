ffbd349d99bd67021763651de6c6dfe42b44e641
Adeola Adetoyese <adeolakautharadetoyese@mitt.ca>
2021-12-11 22:41:53 -0600

    use fetch to request the current weather from openWeather API

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,16 @@
 //api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
 let lat;
 let lon;
+
+const currentConditions = document.getElementById("current-conditions");
+
+const currentWeather = (lat, lon) => {
+  return fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=b4e0b0a6c8793504deea892cdf71dab4`
+  ).then((response) => response.json());
+};
 
 if (!navigator.geolocation) {
   console.log("Geolocation is not supported by your browser");
@@ -10,5 +19,6 @@ if (!navigator.geolocation) {
     lat = position.coords.latitude;
     lon = position.coords.longitude;
     console.log(lat, lon);
+    currentWeather(lat, lon).then((weather) => console.log(weather));
   });
 }
