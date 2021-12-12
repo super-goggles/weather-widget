b7d1ae386e0f33b956f44feb1a88c642f6bd36b1
Adeola Adetoyese <adeolakautharadetoyese@mitt.ca>
2021-12-11 23:08:29 -0600

    update current conditions

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -8,7 +8,7 @@ const currentConditions = document.getElementById("current-conditions");
 
 const currentWeather = (lat, lon) => {
   return fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=b4e0b0a6c8793504deea892cdf71dab4`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=b4e0b0a6c8793504deea892cdf71dab4`
   ).then((response) => response.json());
 };
 
@@ -19,6 +19,19 @@ if (!navigator.geolocation) {
     lat = position.coords.latitude;
     lon = position.coords.longitude;
     console.log(lat, lon);
-    currentWeather(lat, lon).then((weather) => console.log(weather));
+    currentWeather(lat, lon).then(
+      (weather) => {
+        currentConditions.insertAdjacentHTML(
+          "beforeend",
+          `<h2>Current Conditions</h2>
+        <img src="http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png" />
+        <div class="current">
+          <div class="temp">${weather.main.temp}</div>
+          <div class="condition">${weather.weather[0].description}</div>
+        </div>`
+        );
+      }
+      //console.log(weather)
+    );
   });
 }
