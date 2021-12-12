7b3909765cf8575f2fe33ac89d25f4541e302e45
Adeola Adetoyese <adeolakautharadetoyese@mitt.ca>
2021-12-11 22:07:56 -0600

    implement geolocation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,14 @@
 //api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+let lat;
+let lon;
+
+if (!navigator.geolocation) {
+  console.log("Geolocation is not supported by your browser");
+} else {
+  navigator.geolocation.getCurrentPosition((position) => {
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    console.log(lat, lon);
+  });
+}
