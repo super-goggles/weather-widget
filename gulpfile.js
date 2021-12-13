6c443137162009905c64dc4eb2f0786248b6b1c5
Adeola Adetoyese <adeolakautharadetoyese@mitt.ca>
2021-12-12 19:53:26 -0600

    create initial gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
+const { src, dest, series } = require('gulp');
+
+function pagesTask() {
+  return src('src/*.html').pipe(dest('dist'))
+}
+
+function scriptsTask() {
+  return src('src/scripts/*.js').pipe(dest('dist/scripts'));
+}
+
+function stylesTask() {
+  return src('src/styles/*.css').pipe(dest('dist/styles'));
+}
+
 function defaultTask(cb) {
   // place code for your default task here
   cb();
 }
 
-exports.default = defaultTask
+exports.default = series(pagesTask, scriptsTask, stylesTask);
